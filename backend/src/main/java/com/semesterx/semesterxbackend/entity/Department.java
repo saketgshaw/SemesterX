892161288c8000531b9d8925dbd18997dcc38b1a
soumya1708 <soumya.mondal1708@gmail.com>
2026-07-24 10:06:30 +0530

    Integrate backend updates and Department module

--- backend/src/main/java/com/semesterx/semesterxbackend/entity/Department.java
+++ backend/src/main/java/com/semesterx/semesterxbackend/entity/Department.java
@@ -1,3 +1,18 @@
+package com.semesterx.semesterxbackend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "departments")
 @Getter
